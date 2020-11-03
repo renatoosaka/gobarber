cf0cc73cc88ff6390c4c5c4458560f54ae9b8c2e
Renato Hideki Osaka <renato.osaka@hotmail.com>
2020-11-03 16:44:45 -0300

    created avatar field, multer configuration

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -21,5 +27,20 @@ usersRouter.post('/', async (request, response) => {
     });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      return response.json({});
+    } catch (err) {
+      return response.status(400).json({
+        error: err.message,
+      });
+    }
+  },
+);
 
 export default usersRouter;
