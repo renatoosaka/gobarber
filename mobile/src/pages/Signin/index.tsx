75dd072090a95e2f38ef5875c7af227732ad823a
Renato Hideki Osaka <renato.osaka@hotmail.com>
2020-11-11 16:23:23 -0300

    feat: signin page

--- mobile/src/pages/Signin/index.tsx
+++ mobile/src/pages/Signin/index.tsx
@@ -1,7 +1,62 @@
 import React from 'react';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
-const SignIn: React.FC = () => <Container />;
+import logoImg from '../../assets/logo.png';
+
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountText,
+} from './styles';
+
+const SignIn: React.FC = () => (
+  <>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+    >
+      <ScrollView
+        contentContainerStyle={{ flex: 1 }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <Container>
+          <Image source={logoImg} />
+
+          <View>
+            <Title>Faça seu logon</Title>
+          </View>
+
+          <Input name="email" icon="mail" placeholder="E-mail" />
+          <Input name="password" icon="lock" placeholder="Senha" />
+
+          <Button onPress={() => console.log('signin')}>Entrar</Button>
+
+          <ForgotPassword onPress={() => console.log('forgot')}>
+            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+          </ForgotPassword>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+
+    <CreateAccountButton onPress={() => console.log('create')}>
+      <Icon name="log-in" size={20} color="#ff9000" />
+      <CreateAccountText>Criar uma conta</CreateAccountText>
+    </CreateAccountButton>
+  </>
+);
 
 export default SignIn;
