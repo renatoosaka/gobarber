6165dba1ddee6193a7c4b4ded278ee2e39f1c622
Renato Hideki Osaka <renato.osaka@hotmail.com>
2020-11-11 16:31:31 -0300

    feat: signup page

--- mobile/src/pages/Signup/index.tsx
+++ mobile/src/pages/Signup/index.tsx
@@ -1,7 +1,57 @@
 import React from 'react';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
-const SignUp: React.FC = () => <Container />;
+import logoImg from '../../assets/logo.png';
+
+import {
+  Container,
+  Title,
+  BackToSignInButton,
+  BackToSignInText,
+} from './styles';
+
+const SignUp: React.FC = () => (
+  <>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+    >
+      <ScrollView
+        contentContainerStyle={{ flex: 1 }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <Container>
+          <Image source={logoImg} />
+
+          <View>
+            <Title>Crie sua conta</Title>
+          </View>
+
+          <Input name="name" icon="user" placeholder="Nome" />
+          <Input name="email" icon="mail" placeholder="E-mail" />
+          <Input name="password" icon="lock" placeholder="Senha" />
+
+          <Button onPress={() => console.log('signin')}>Entrar</Button>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+
+    <BackToSignInButton onPress={() => console.log('create')}>
+      <Icon name="arrow-left" size={20} color="#f4ede8" />
+      <BackToSignInText>Voltar para logon</BackToSignInText>
+    </BackToSignInButton>
+  </>
+);
 
 export default SignUp;
