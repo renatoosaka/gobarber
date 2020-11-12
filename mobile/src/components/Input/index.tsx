44bb4fb6917bae2587d1ff2409fc7f19edf77766
Renato Hideki Osaka <renato.osaka@hotmail.com>
2020-11-12 11:17:49 -0300

    refactor: removed deprecated input code

--- mobile/src/components/Input/index.tsx
+++ mobile/src/components/Input/index.tsx
@@ -20,75 +20,70 @@ interface InputValueReference {
   value: string;
 }
 
-interface InputRef {
-  focus(): void;
-}
+const Input: React.FC<InputProps> = forwardRef(
+  ({ name, icon, ...rest }, ref) => {
+    const inputElementRef = useRef<any>(null);
 
-const Input: React.RefForwardingComponent<InputRef, InputProps> = (
-  { name, icon, ...rest },
-  ref,
-) => {
-  const inputElementRef = useRef<any>(null);
+    const { registerField, defaultValue, fieldName, error } = useField(name);
+    const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
 
-  const { registerField, defaultValue, fieldName, error } = useField(name);
-  const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
+    const [isFocused, setIsFocused] = useState(false);
+    const [isFilled, setIsFilled] = useState(false);
 
-  const [isFocused, setIsFocused] = useState(false);
-  const [isFilled, setIsFilled] = useState(false);
+    const handleInputFocus = useCallback(() => {
+      setIsFocused(true);
+    }, []);
 
-  const handleInputFocus = useCallback(() => {
-    setIsFocused(true);
-  }, []);
+    const handleInputBlur = useCallback(() => {
+      setIsFocused(false);
 
-  const handleInputBlur = useCallback(() => {
-    setIsFocused(false);
+      setIsFilled(!!inputValueRef.current.value);
+    }, []);
 
-    setIsFilled(!!inputValueRef.current.value);
-  }, []);
-
-  useImperativeHandle(ref, () => ({
-    focus() {
-      inputElementRef.current.focus();
-    },
-  }));
-
-  useEffect(() => {
-    registerField<string>({
-      name: fieldName,
-      ref: inputValueRef.current,
-      path: 'value',
-      setValue(ref: any, value) {
-        inputValueRef.current.value = value;
-        inputElementRef.current.setNativeProps({ text: value });
+    useImperativeHandle(ref, () => ({
+      focus() {
+        inputElementRef.current.focus();
       },
-      clearValue() {
-        inputValueRef.current.value = '';
-        inputElementRef.current.clear();
-      },
-    });
-  }, [fieldName, registerField]);
+    }));
 
-  return (
-    <Container isFocused={isFocused} isErrored={!!error}>
-      <Icon
-        name={icon}
-        size={20}
-        color={isFocused || isFilled ? '#ff9000' : '#666360'}
-      />
-      <TextInput
-        ref={inputElementRef}
-        keyboardAppearance="dark"
-        placeholderTextColor="#666360"
-        defaultValue={defaultValue}
-        onFocus={handleInputFocus}
-        onBlur={handleInputBlur}
-        onChangeText={value => {
+    useEffect(() => {
+      registerField<string>({
+        name: fieldName,
+        ref: inputValueRef.current,
+        path: 'value',
+        setValue(ref: any, value) {
           inputValueRef.current.value = value;
-        }}
-        {...rest}
-      />
-    </Container>
-  );
-};
+          inputElementRef.current.setNativeProps({ text: value });
+        },
+        clearValue() {
+          inputValueRef.current.value = '';
+          inputElementRef.current.clear();
+        },
+      });
+    }, [fieldName, registerField]);
 
-export default forwardRef(Input);
+    return (
+      <Container isFocused={isFocused} isErrored={!!error}>
+        <Icon
+          name={icon}
+          size={20}
+          color={isFocused || isFilled ? '#ff9000' : '#666360'}
+        />
+        <TextInput
+          ref={inputElementRef}
+          keyboardAppearance="dark"
+          placeholderTextColor="#666360"
+          defaultValue={defaultValue}
+          onFocus={handleInputFocus}
+          onBlur={handleInputBlur}
+          onChangeText={value => {
+            inputValueRef.current.value = value;
+          }}
+          {...rest}
+        />
+      </Container>
+    );
+  },
+);
+
+export default Input;
