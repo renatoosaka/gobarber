67be760735e8b0341a0adf7a911902468e3fc5ed
Renato Hideki Osaka <renato.osaka@hotmail.com>
2020-11-24 11:28:13 -0300

    test: reset and send forgot email password

---
+++ backend/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -0,0 +1,30 @@
+import { v4 } from 'uuid';
+
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
+
+class FakeUserTokensRepository implements IUserTokensRepository {
+  userTokens: UserToken[] = [];
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = new UserToken();
+
+    Object.assign(userToken, {
+      id: v4(),
+      token: v4(),
+      user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.userTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    return this.userTokens.find(userToken => userToken.token === token);
+  }
+}
+
+export default FakeUserTokensRepository;
---
+++ backend/src/modules/users/services/SendForgotEmailPasswordService.ts
@@ -0,0 +1,41 @@
+import { inject, injectable } from 'tsyringe';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import AppError from '@shared/errors/AppError';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotEmailPasswordService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists.');
+    }
+
+    await this.userTokensRepository.generate(user.id);
+
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperacao de senha recebido',
+    );
+  }
+}
+
+export default SendForgotEmailPasswordService;
