bf7fecc20a71959a843058343596d45be455439e
Renato Hideki Osaka <renato.osaka@hotmail.com>
2020-11-25 08:58:32 -0300

    feat: list providers

--- backend/src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ backend/src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -3,9 +3,22 @@ import { v4 } from 'uuid';
 import IUserRepository from '@modules/users/repositories/IUsersRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 import User from '@modules/users/infra/typeorm/entities/User';
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
 
 class FakeUsersRepository implements IUserRepository {
   private users: User[] = [];
+
+  public async findAllProviders({
+    except_user_id,
+  }: IFindAllProvidersDTO): Promise<User[]> {
+    let { users } = this;
+
+    if (except_user_id) {
+      users = this.users.filter(user => user.id !== except_user_id);
+    }
+
+    return users;
+  }
 
   public async findById(id: string): Promise<User | undefined> {
     return this.users.find(user => user.id === id);
