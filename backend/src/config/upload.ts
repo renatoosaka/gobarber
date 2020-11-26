a269b1bbaab3bc6c44eaa897e33c5f19f74ac6fb
Renato Hideki Osaka <renato.osaka@hotmail.com>
2020-11-26 14:16:20 -0300

    feat: amazon s3 storage provider

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -1,19 +1,44 @@
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+
+  tmpFolder: string;
+  uploadFolder: string;
+
+  multer: {
+    storage: StorageEngine;
+  };
+
+  config: {
+    aws: {
+      bucket: string;
+    };
+  };
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER || `disc`,
   tmpFolder,
   uploadFolder: path.resolve(tmpFolder, 'uploads'),
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex');
-      const fileName = `${fileHash}-${file.originalname}`;
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
+        const fileName = `${fileHash}-${file.originalname}`;
 
-      return callback(null, fileName);
+        return callback(null, fileName);
+      },
+    }),
+  },
+  config: {
+    aws: {
+      bucket: process.env.AWS_S3_BUCKET_NAME || 'bucket-name',
     },
-  }),
-};
+  },
+} as IUploadConfig;
--- backend/src/modules/users/infra/typeorm/entities/User.ts
+++ backend/src/modules/users/infra/typeorm/entities/User.ts
@@ -8,6 +8,8 @@ import {
 
 import { Exclude, Expose } from 'class-transformer';
 
+import uploadConfig from '@config/upload';
+
 @Entity('users')
 class User {
   @PrimaryGeneratedColumn('uuid')
@@ -33,8 +35,19 @@ class User {
   updated_at: Date;
 
   @Expose({ name: 'avatar_url' })
-  getAvatarUrl(): string {
-    return this.avatar && `${process.env.APP_API_URL}/files/${this.avatar}`;
+  getAvatarUrl(): string | null {
+    if (!this.avatar) {
+      return null;
+    }
+
+    switch (uploadConfig.driver) {
+      case 'disk':
+        return `${process.env.APP_API_URL}/files/${this.avatar}`;
+      case 's3':
+        return `https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${this.avatar}`;
+      default:
+        return null;
+    }
   }
 }
 
