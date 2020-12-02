dd2ca574696368c63c101f588ff816e17d690704
Renato Hideki Osaka <renato.osaka@hotmail.com>
2020-12-02 09:57:31 -0300

    feat: dashboard header

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import React, { useCallback } from 'react';
 
 import { Button } from 'react-native';
 import { useAuth } from '../../hooks/auth';
 
-import { Container } from './styles';
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+} from './styles';
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { navigate } = useNavigation();
+
+  const { signOut, user } = useAuth();
+
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile');
+  }, [navigate]);
+
   return (
     <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo,
+          {'\n'}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar
+            source={{
+              uri: user.avatar_url,
+            }}
+          />
+        </ProfileButton>
+      </Header>
       <Button title="Sair" onPress={signOut} />
     </Container>
   );
